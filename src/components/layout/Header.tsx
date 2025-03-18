4f229d38c93496157f9ad7f127ca2ee54f231b95
M.F.M <mfmfazrin1986@gmail.com>
2025-03-18 23:32:32 +0300

    first commit

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,73 @@
+import { useState } from 'react';
+import { AppBar, Toolbar, Typography, Button, IconButton, Menu, MenuItem, Box, useMediaQuery, useTheme } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <AppBar position="static" color="primary">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
+            MovieReco
+          </Link>
+        </Typography>
+        
+        {isMobile ? (
+          <>
+            <IconButton
+              size="large"
+              edge="end"
+              color="inherit"
+              aria-label="menu"
+              onClick={handleMenu}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose} component={Link} to="/">Home</MenuItem>
+              <MenuItem onClick={handleClose} component={Link} to="/rankings">Rankings</MenuItem>
+              <MenuItem onClick={handleClose} component={Link} to="/streaming">Streaming</MenuItem>
+              <MenuItem onClick={handleClose} component={Link} to="/profile">Profile</MenuItem>
+            </Menu>
+          </>
+        ) : (
+          <Box sx={{ display: 'flex' }}>
+            <Button color="inherit" component={Link} to="/">Home</Button>
+            <Button color="inherit" component={Link} to="/rankings">Rankings</Button>
+            <Button color="inherit" component={Link} to="/streaming">Streaming</Button>
+            <Button color="inherit" component={Link} to="/profile">Profile</Button>
+          </Box>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
